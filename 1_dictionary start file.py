49ecb8d691c810e97e13b4c22571150392be9b3f
campbell <campbell.com>
2022-09-08 12:14:09 -0500

    dictionary learning

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,8 +10,20 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook))
 
+phone = phonebook ['Chris']
 
+print(phone)
+
+print(len(phonebook))
+
+mydictionary = dict(m=8, n=9)
+print(mydictionary)
+
+mydict = {}
+print(mydict)
 
 
 print()
@@ -19,7 +31,7 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
@@ -27,7 +39,12 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+name = 'Chris'
 
+if name in phonebook: 
+    print(phonebook[name])
+else:
+    print(name,'not found')
 
 
 
@@ -40,14 +57,15 @@ print()
 
 
 
-
-
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
 
-
+print(phonebook)
+phonebook ['Chris'] = '555-0123'
+phonebook ['Joe'] = '555-4444'
+print(phonebook)
 
 
 print()
@@ -58,11 +76,13 @@ print()
 
 
 
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+
+#del phonebook['Chris']
+#print(phonebook)
 
 
 
@@ -74,13 +94,27 @@ print()
 
 
 
-
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook:
+    print(key)
+    print(phonebook[key]) #this will give the values as well
 
+#automatically iterates through keys. if you wan to look through values you will have to give a different command
+
+for value in phonebook.values():
+    print(value)
+
+
+for k,v in phonebook.items():
+    print('key:',k,'value:', v)
+
+
+for tuple in phonebook.items():
+    print(tuple)
 
 
 
@@ -91,13 +125,18 @@ print()
 
 
 
-
 print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
 
 
+phone = phonebook.get('Chris', 'ket not found')
+print(phone)
+
+
+#phonebook.clear()
+#print(phonebook)
 
 
 
@@ -111,7 +150,12 @@ print()
 print('*****  start section 7 - using pop method ********')
 print()
 
+#'pop' will take take it out of the dictionalry in your output
 
+#remove = phonebook.pop('Chris', 'key not found')
+
+#print(remove)
+#print(phonebook)
 
 
 
@@ -128,6 +172,12 @@ print()
 
 
 
+#a = phonebook.popitem()
+
+#print(a)
+#print(phonebook)
+
+#there is an error where it doesn't randomize it, it picks the last vale in your dictionary 
 
 
 
@@ -142,7 +192,15 @@ print('*****  start section 9 - using random and converting to list ********')
 print()
 
 
+list_of_keys = list(phonebook)
+print(list_of_keys)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
 
+#these do the same thing, the line below is just more effecient 
+
+print(phonebook[random.choice(list(phonebook))])
 
 
 print()
@@ -150,7 +208,7 @@ print('*****  end section 9 ********')
 print()
 
 
-'''
+
 
 
 
